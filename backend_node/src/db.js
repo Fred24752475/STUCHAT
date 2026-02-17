e4c1135273775cfe5f81ebec39093529cd2ae525
Fred24752475 <fredrickokyere777@gmail.com>
2026-02-17 23:15:40 +0000

    Revert SSL config to default

--- backend_node/src/db.js
+++ backend_node/src/db.js
@@ -1,10 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:Fred678rick2475@db.usvythgjalhgmjuvxqsa.supabase.co:5432/postgres?sslmode=require',
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:Fred678rick2475@db.usvythgjalhgmjuvxqsa.supabase.co:5432/postgres?sslmode=require'
 });
 
 const query = async (text, params) => {
